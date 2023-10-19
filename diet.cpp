4b392ae1d9d0930d66d9ffcf8d25956b8c36f807
nishathahir <nishathahir12@gmail.com>
2023-10-19 12:20:42 +0530

    remove using

--- diet.cpp
+++ diet.cpp
@@ -3,7 +3,6 @@
 #include<stdlib.h>
 #include"diet.h"
 
-using namespace std;
 
 void Diet::Risk() const
 {
@@ -14,28 +13,28 @@ void Diet::Risk() const
         file.open("underweight_risk.txt");
         if(!file)
         {
-            cout<<"\nERROR"<<endl;
-            cout<<"File Not Found"<<endl;
+            std::cout<<"\nERROR"<<endl;
+            std::cout<<"File Not Found"<<endl;
         }
         else
         {
             string line;
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
             while(file.good())
             {
                 getline(file,line);
-                cout<<"\n\t\t\t\t"<<line<<endl;
+                std::cout<<"\n\t\t\t\t"<<line<<endl;
             }
             file.close();
         }
@@ -45,28 +44,28 @@ void Diet::Risk() const
         file.open("normal_maintain.txt");
         if(!file)
         {
-            cout<<"\nERROR"<<endl;
-            cout<<"File Not Found"<<endl;
+            std::cout<<"\nERROR"<<endl;
+            std::cout<<"File Not Found"<<endl;
         }
         else
         {
             string line;
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
             while(file.good())
             {
                 getline(file,line);
-                cout<<"\n\t\t\t\t"<<line<<endl;
+                std::cout<<"\n\t\t\t\t"<<line<<endl;
             }
             file.close();
         }
@@ -76,28 +75,28 @@ void Diet::Risk() const
         file.open("overweight_risk.txt");
         if(!file)
         {
-            cout<<"\nERROR"<<endl;
-            cout<<"File Not Found"<<endl;
+            std::cout<<"\nERROR"<<endl;
+            std::cout<<"File Not Found"<<endl;
         }
         else
         {
             string line;
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
-            cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
+            std::cout<<"\n";
             while(file.good())
             {
                 getline(file,line);
-                cout<<"\n\t\t\t\t"<<line<<endl;
+                std::cout<<"\n\t\t\t\t"<<line<<endl;
             }
             file.close();
         }
@@ -107,24 +106,24 @@ void Diet::Risk() const
 void Diet::FoodTimings() const
 {
     system("open");
-    cout<<"\n";
-    cout<<"\n";
-    cout<<"\n\t\t\t\t--------------------------------------------------BEST TIME TO EAT--------------------------------------------------"<<endl;
-    cout<<"\n\t\t\t\tBreakfast :"<<endl;
-    cout<<"\n\t\t\t\t\t- The sooner you eat breakfast after you wake up, the better it is for your metabolism."<<endl;
-    cout<<"\n\t\t\t\t\t- Never skip your breakfast"<<endl;
-    cout<<"\n\t\t\t\t\t- Breakfast can be between 7 am to 9 am or within two hours of getting up."<<endl;
-    cout<<"\n\t\t\t\tLunch:"<<endl;
-    cout<<"\n\t\t\t\t\t- Ideal time to have lunch is 12.45pm"<<endl;
-    cout<<"\n\t\t\t\t\t- Try and keep a gap of 4 hours between your breakfast and lunch"<<endl;
-    cout<<"\n\t\t\t\tDinner:"<<endl;
-    cout<<"\n\t\t\t\t\t- The ideal time to have dinner is before 7pm"<<endl;
-    cout<<"\n\t\t\t\t\t- You should keep a gap of 3 hours between your dinner and bedtime"<<endl;
-    cout<<"\n\t\t\t\tSnack Time:"<<endl;
-    cout<<"\n\t\t\t\t\t- For instance, if you eat breakfast at 7 a.m. and lunch at noon, include a mid-morning snack at 10 a.m."<<endl;
-    cout<<"\n\t\t\t\t\t- If you eat breakfast at 8 a.m. and lunch at noon, you probably don't need a snack."<<endl;
-    cout<<"\n\t\t\t\t\t- Basically, you want to eat every 2-3 hours,depending on your schedule"<<endl;
-    cout<<"\n\t\t\t\t-------------------------------------------------------------------------------------------------------------------"<<endl;
+    std::cout<<"\n";
+    std::cout<<"\n";
+    std::cout<<"\n\t\t\t\t--------------------------------------------------BEST TIME TO EAT--------------------------------------------------"<<endl;
+    std::cout<<"\n\t\t\t\tBreakfast :"<<endl;
+    std::cout<<"\n\t\t\t\t\t- The sooner you eat breakfast after you wake up, the better it is for your metabolism."<<endl;
+    std::cout<<"\n\t\t\t\t\t- Never skip your breakfast"<<endl;
+    std::cout<<"\n\t\t\t\t\t- Breakfast can be between 7 am to 9 am or within two hours of getting up."<<endl;
+    std::cout<<"\n\t\t\t\tLunch:"<<endl;
+    std::cout<<"\n\t\t\t\t\t- Ideal time to have lunch is 12.45pm"<<endl;
+    std::cout<<"\n\t\t\t\t\t- Try and keep a gap of 4 hours between your breakfast and lunch"<<endl;
+    std::cout<<"\n\t\t\t\tDinner:"<<endl;
+    std::cout<<"\n\t\t\t\t\t- The ideal time to have dinner is before 7pm"<<endl;
+    std::cout<<"\n\t\t\t\t\t- You should keep a gap of 3 hours between your dinner and bedtime"<<endl;
+    std::cout<<"\n\t\t\t\tSnack Time:"<<endl;
+    std::cout<<"\n\t\t\t\t\t- For instance, if you eat breakfast at 7 a.m. and lunch at noon, include a mid-morning snack at 10 a.m."<<endl;
+    std::cout<<"\n\t\t\t\t\t- If you eat breakfast at 8 a.m. and lunch at noon, you probably don't need a snack."<<endl;
+    std::cout<<"\n\t\t\t\t\t- Basically, you want to eat every 2-3 hours,depending on your schedule"<<endl;
+    std::cout<<"\n\t\t\t\t-------------------------------------------------------------------------------------------------------------------"<<endl;
     system("pause");
     system("cls");
 }
